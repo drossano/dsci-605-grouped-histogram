6e7b5010485d5bb4e5337fca8d2029cd82fbb6bc
Dean <72749793+drossano@users.noreply.github.com>
2024-01-15 11:23:23 -0500

    Change both histograms to be dodged

--- DeanRossano_GroupHistogram.R
+++ DeanRossano_GroupHistogram.R
@@ -16,7 +16,7 @@ range_col = c("green", "blue", "red")
 
 ## Plot Graph with Year as Color Vector
 ggplot(df, aes(x = CrimeRate, color=as.factor(Year), fill = as.factor(Year))) +
-  geom_histogram() +
+  geom_histogram(position = "dodge", alpha=0.5, bins=10) +
   scale_color_manual(values = year_col)+
   scale_fill_manual(values = year_col)+
   labs(title= "Crime Rate by Year", color= "Year", fill="Year") + ylab("Count") +
@@ -25,8 +25,8 @@ ggplot(df, aes(x = CrimeRate, color=as.factor(Year), fill = as.factor(Year))) +
 
 ## Plot Graph with RangeGroup as Color Vector
 ggplot(df, aes(x = CrimeRate, color=as.factor(RangeGroup), fill = as.factor(RangeGroup))) +
-  geom_histogram() +
+  geom_histogram(position = "dodge", alpha=0.5, bins=10) +
   scale_color_manual(values = range_col)+
   scale_fill_manual(values = range_col)+
   labs(tite= "Crime Rate by Range", color= "Range", fill="Range") + ylab("Count") +
-  xlab("Crime Rate")
+  xlab("Crime Rate Range")
